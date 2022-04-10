8757a83a760d7409bdc8c41fd217efb85820bb3e
Elmir <94553249+RussianPlay@users.noreply.github.com>
2022-04-10 05:41:56 +0300

    feature 0.8

---
+++ WEB_Alice_2/alice_and_translator/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+import logging
+import json
+import requests
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+translator_url = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get"
+headers = {
+    'x-rapidapi-key': "286a48c82cmsh83930d71ba2fb4ap124a57jsndefca9bce98d",
+    'x-rapidapi-host': "translated-mymemory---translation-memory.p.rapidapi.com"
+}
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info('Request: %r', request.json)
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info('Response: %r', response)
+    return json.dumps(response)
+
+
+def handle_dialog(res, req):
+    text = req["request"]["command"].split(" слово ")[-1]
+    querystring = {"langpair": "ru|en",
+                   "q": text,
+                   "mt": "1",
+                   "onlyprivate": "0",
+                   "de": "ahmadullinta@mail.ru",
+                   "format": "json"}
+    response = requests.request("GET", translator_url, headers=headers, params=querystring)
+    res["response"]["text"] = response.json()["responseData"]["translatedText"]
+
+
+if __name__ == '__main__':
+    app.run()
+
+
